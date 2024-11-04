840e1ba334ea62109b94588faccc268103bad92f
alazeprt <nono135246@126.com>
2024-11-04 16:58:44 +0800

    Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,11 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
