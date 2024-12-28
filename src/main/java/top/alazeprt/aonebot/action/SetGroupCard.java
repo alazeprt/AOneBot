a7b20a79ecb828bc4a47e5effc37d162929647e1
alazeprt <nono135246@126.com>
2024-12-28 22:14:50 +0800

    Update version

---
+++ src/main/java/top/alazeprt/aonebot/action/SetGroupCard.java
@@ -0,0 +1,29 @@
+package top.alazeprt.aonebot.action;
+
+import top.alazeprt.aonebot.util.MapUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static top.alazeprt.aonebot.BotClient.gson;
+
+public class SetGroupCard extends PostAction {
+    private final long groupId;
+    private final long userId;
+    private final String card;
+
+    public SetGroupCard(long groupId, long userId, String card) {
+        this.groupId = groupId;
+        this.userId = userId;
+        this.card = card;
+    }
+
+    @Override
+    public String getData() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("action", "set_group_card");
+        map.put("params", MapUtil.of("group_id", groupId, "user_id", userId, "card", card));
+        map.put("echo", "aob_" + System.currentTimeMillis()%10000);
+        return gson.toJson(map);
+    }
+}
